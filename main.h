b26f1f76ff57a8b4ecab62113ff4d3d23e5feeea
sabmr24 <saadiabouz478@gmail.com>
2023-07-23 14:25:37 -0700

    betty style in the header file

--- main.h
+++ main.h
@@ -2,18 +2,17 @@
 #define _PRINTF_HEADER
 #include <stdarg.h>
 
-/**
- * struct specs - struct for specifiers
- * @spec: the specifier
- * @func: the function associated
- */
-
 void _puts(char *str);
 int _putchar(char c);
 int _print_a_char(va_list the_args, int count);
 int _print_a_string(va_list the_args, int count);
 int _print_a_percent(va_list the_args, int count);
 
+/**
+ * struct specs - struct for specifiers
+ * @spec: the specifier
+ * @func: the function associated
+ */
 typedef struct specs
 {
 	char *spec;
