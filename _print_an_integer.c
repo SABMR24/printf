22b3daf6e3fe2d895ca9a43ea5bdbbb0084ac221
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-25 23:46:41 +0100

    integer

--- _print_an_integer.c
+++ _print_an_integer.c
@@ -1,76 +1,50 @@
 #include "main.h"
-#include <stdint.h>
 
-int _absol(int n)
+/**
+ * printf_integer - prints intiger number
+ * @args: number arguements
+ * @printed: the printed characters
+ * Return: printed charcaters
+ */
+
+int _print_an_integer(va_list args, int count)
 {
-	if (n < 0)
-		return (-n);
-	return (n);
-}
+    int num = va_arg(args, int);
+    int len = 0;
+    int num1 = num;
 
+    if (num == 0)
+    {
+        _putchar('0');
+        count++;
+        return (count);
+    }
+    if (num < 0)
+    {
+        _putchar('-');
+        num = -num;
+        count++;
+    }
+    while (num1 != 0)
+    {
+        num1 /= 10;
+        len++;
+    }
+    while (len > 0)
+    {
+        int x = 1;
+        int i = 1;
 
-int cntdigits(int n)
-{
-	int cntdigits = 0;
-	
-	while (n != 0)
-	{
-		n /= 10;
-		cntdigits++;
-	}
-	return (cntdigits);
-}
-
-int _pow(int x, int y)
-{
-	int i, result = 1;
-	
-	for (i = 0; i < y; i++)
-		result *= x;
-	return (result);
-}
-
-
-int _print_an_integer(va_list the_args, int count)
-{
-	int num = va_arg(the_args, int);
-	int cntdig = cntdigits(num);
-	int i, e, f;
-	
-	if (num < 0)
-	{
-		_putchar('-');
-		count++;
-		num = _absol(num);
-	}
-	if (num == 0)
-	{
-		_putchar('0');
-		count++;
-	}
-	if (num == -2147483648)
-	{
-		_putchar('2');
-		_putchar('1');
-		_putchar('4');
-		_putchar('7');
-		_putchar('4');
-		_putchar('8');
-		_putchar('3');
-		_putchar('6');
-		_putchar('4');
-		_putchar('8');
-		count += 10;
-	}
-	if (num > 0)
-	{
-		for (i = cntdig - 1; i >= 0; i--)
-		{
-			e = num / _pow(10, i);
-			f = e % 10;
-			_putchar(f + '0');
-			count++;
-		}
-	}
-	return (count);
+        while (i < len)
+        {
+            x *= 10;
+            i++;
+        }
+        _putchar(num / x + '0');
+        count++;
+        num %= x;
+        len--;
+    }
+    return (count);
+    
 }
