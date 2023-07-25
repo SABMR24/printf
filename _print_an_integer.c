4297576ac302fb3fcd4673a17f6cb1b94ec56764
sabmr24 <saadiabouz478@gmail.com>
2023-07-25 14:54:04 -0700

    int

--- _print_an_integer.c
+++ _print_an_integer.c
@@ -1,52 +1,76 @@
+```c
 #include "main.h"
 #include <stdint.h>
 
-/**
- * _print_an_integer - prints an integer
- * @the_args: the va_list that holds the integer to be printed
- * @count: the counter of already printed characters.
- * Return: it adds the number of printed characters to count.
- */
+int _absol(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+
+int cntdigits(int n)
+{
+	int cntdigits = 0;
+	
+	while (n != 0)
+	{
+		n /= 10;
+		cntdigits++;
+	}
+	return (cntdigits);
+}
+
+int _pow(int x, int y)
+{
+	int i, result = 1;
+	
+	for (i = 0; i < y; i++)
+		result *= x;
+	return (result);
+}
+
 
 int _print_an_integer(va_list the_args, int count)
 {
-	int number = va_arg(the_args, int);
-	int i, len, digits[20];
-
-	if (!number)
-	{
-		_puts("0");
-	return (1); }
-	if (number == -2147483648)
-	{
-		_puts("-2147483648");
-	return (11); }
-	if (number == 0)
-	{
-		_putchar('0');
-		count++; }
-
-	if (number < 0)
+	int num = va_arg(the_args, int);
+	int cntdig = cntdigits(num);
+	int i, e, f;
+	
+	if (num < 0)
 	{
 		_putchar('-');
 		count++;
-		number = -number; }
-
-	if (number > 0)
+		num = _absol(num);
+	}
+	if (num == 0)
 	{
-		for (len = 0; number != 0; len++)
+		_putchar('0');
+		count++;
+	}
+	if (num == -2147483648)
+	{
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('8');
+		count += 10;
+	}
+	if (num > 0)
+	{
+		for (i = cntdig - 1; i >= 0; i--)
 		{
-			digits[len] = number % 10;
-			number = number / 10; }
-
-		for (i = len - 1; i >= 0; i--)
-		{
-			if (digits[i] == 0)
-				continue;
-				else
-				{
-				_putchar(digits[i] + '0');
-			count++; }
+			e = num / _pow(10, i);
+			f = e % 10;
+			_putchar(f + '0');
+			count++;
 		}
 	}
 	return (count);
