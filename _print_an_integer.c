d361a1102d4c6b4dccd7472c1a0e911325febd1e
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-26 01:12:14 +0100

    rot13

--- _print_an_integer.c
+++ _print_an_integer.c
@@ -1,50 +1,47 @@
 #include "main.h"
 
 /**
- * printf_integer - prints intiger number
+ * _print_an_integer - prints intiger number
  * @args: number arguements
- * @printed: the printed characters
- * Return: printed charcaters
+ * @count: the count
+ * Return: count of printed charcaters
  */
 
 int _print_an_integer(va_list args, int count)
 {
-    int num = va_arg(args, int);
-    int len = 0;
-    int num1 = num;
+	int num = va_arg(args, int);
+	int len = 0, num1 = num;
 
-    if (num == 0)
-    {
-        _putchar('0');
-        count++;
-        return (count);
-    }
-    if (num < 0)
-    {
-        _putchar('-');
-        num = -num;
-        count++;
-    }
-    while (num1 != 0)
-    {
-        num1 /= 10;
-        len++;
-    }
-    while (len > 0)
-    {
-        int x = 1;
-        int i = 1;
+	if (num == 0)
+	{
+		_putchar('0');
+		count++;
+		return (count); }
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	count++; }
+	if (num == -2147483648)
+	{
+		_puts("2147483648");
+		count += 10;
+		return (count); }
+	while (num1 != 0)
+	{
+		num1 /= 10;
+	len++; }
+	while (len > 0)
+	{
+		int x = 1, i = 1;
 
-        while (i < len)
-        {
-            x *= 10;
-            i++;
-        }
-        _putchar(num / x + '0');
-        count++;
-        num %= x;
-        len--;
-    }
-    return (count);
-    
+	while (i < len)
+	{
+		x *= 10;
+		i++; }
+		_putchar(num / x + '0');
+		count++;
+		num %= x;
+	len--; }
+	return (count);
 }
